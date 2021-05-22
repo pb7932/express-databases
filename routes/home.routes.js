668dbd11f9c244c476f85bfa8dcaa113119fcbf0
pb7932 <pebe1928@gmail.com>
2021-05-22 15:37:26 +0200

    Removing the student list from home view, implementing main fucntionality

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -5,8 +5,32 @@ const repo = require('../db/index');
 router.get('/', async function( req, res, next ) {
     const predmeti = await (await repo.query('SELECT * FROM predmet')).rows;
     const studenti = await ( await repo.query('SELECT * FROM student LIMIT 200')).rows;
-    
-    res.render('home', {predmeti: predmeti, studenti: studenti});
+    console.log(studenti);
+    res.render('home', {predmeti: predmeti, studenti: studenti, predStud: [], studOcj: []});
+});
+
+router.get('/:sp([0-9]{1,4})', async function( req, res, next ) {
+    const sp = req.params.sp;
+    const predmeti = await (await repo.query('SELECT * FROM predmet')).rows;
+    const predStud = await ( await repo.query(`SELECT student.* FROM upisanpredmet 
+                                                NATURAL JOIN predmeT
+                                                NATURAL JOIN student
+                                                WHERE predmet.sifpredmet = $1 LIMIT 200`, [sp])).rows;
+
+    res.render('home', {predmeti: predmeti, predStud: predStud, studOcj: [], sp: sp});
+});
+
+router.get('/:sp([0-9]{1,4})/:ss([0-9]{1,11})', async function( req, res, next ) {
+    const sp = req.params.sp;
+    const ss = `${req.params.ss}`;
+    const predmeti = await (await repo.query('SELECT * FROM predmet')).rows;
+    const studOcj = await ( await repo.query(`SELECT student.*, ocjena, sifpredmet, nazpredmet 
+                                            FROM ispit 
+                                            NATURAL JOIN student 
+                                            NATURAL JOIN predmet 
+                                            WHERE ispit.jmbag = $1`, [ss])).rows;
+                                            
+    res.render('home', {predmeti: predmeti, predStud: [], studOcj: studOcj, sp: sp});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.urlencoded({extended: true}));
 app.use('/', homeRouter);
 
